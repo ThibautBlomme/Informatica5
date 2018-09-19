d4d28589be6796763d4d3819cf88c5ac986dfb17
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-09-19 11:39:10 +0200

    Oppervlakte Cirkel

---
+++ 04-Variabelen/test.py
@@ -0,0 +1,15 @@
+#vraag coëfficiënten bij x^2, x^1 en x^0 van een tweedegraadsfunctie f(x) = ax^2 + bx^1 + cx^0
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+#bereken de discriminant
+discriminant = b ** 2 - (4 * a * c)
+
+#uitvoer
+print('discriminant = ', discriminant)
+
+
+
+#--------------------------------------
+
