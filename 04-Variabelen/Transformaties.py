60af795e09f14eca494d06a5ae7232296aaef091
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-09-22 18:33:13 +0200

    Transformaties

---
+++ 04-Variabelen/Transformaties.py
@@ -0,0 +1,11 @@
+a = float(input('Geef een waarde voor a: '))
+b = float(input('Geef een waarde voor b: '))
+c = 3
+d = 4
+
+fx = 'f(x) = 2(x - 3)^2 + 4'
+gx = 'g(x) = 2(x - ' + str(int(a + c)) + ')^2 + ' + str(int(b + d))
+
+
+print(fx)
+print(gx)
