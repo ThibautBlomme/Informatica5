4dd7fc5eaf126d722b9ff9a1be05a20d0a0cb996
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-09-25 19:53:09 +0200

    Wisselgeld

--- 04-Variabelen/Wisselgeld.py
+++ 04-Variabelen/Wisselgeld.py
@@ -25,4 +25,4 @@ uitkomst5 = (rest4 // e)
 uitkomst6 = (rest5 // f)
 uitkomst7 = (rest6 // g)
 
-print(str(int(bedrag)) + 'cent kan je omwisselen in' + str(int(uitkomst1 + uitkomst2 + uitkomst3 + uitkomst4 + uitkomst5 + uitkomst6 + uitkomst7)) + ' muntstukken')
+print(str(int(bedrag)) + ' cent kan je omwisselen in ' + str(int(uitkomst1 + uitkomst2 + uitkomst3 + uitkomst4 + uitkomst5 + uitkomst6 + uitkomst7)) + ' muntstukken')
