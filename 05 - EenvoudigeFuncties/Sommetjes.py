4fc897f67b834b65ab3686aa40c40a1dc8b90b46
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-10-01 23:16:31 +0200

    Sommetjes

---
+++ 05 - EenvoudigeFuncties/Sommetjes.py
@@ -0,0 +1,15 @@
+#invoer
+a = float(input('Geef een waarde voor a: '))
+b = float(input('Geef een waarde voor b: '))
+i = 0
+
+print(str('{:>6.0f}'.format(a * pow(10,i)) ' + ' ('{:<6.0f}'.format(b * pow(10,i)))
+i += 1
+print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
+i += 1
+print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
+i += 1
+print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
+
+
+
