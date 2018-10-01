d1018891db5db25567c133932ab5d7e33f872def
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-10-01 22:42:19 +0200

    Absolute waarde

--- 05 - EenvoudigeFuncties/AbsoluteWaarde.py
+++ 05 - EenvoudigeFuncties/AbsoluteWaarde.py
@@ -1,4 +1,8 @@
 x = float(input('Geef een waarde voor x: '))
 y = float(input('Geef een waarde voor y: '))
 
-print (abs(abs(x) - (abs(y)))) + str('\N{LESS-THAN OR EQUAL TO}') + (abs(x - y)
+print(str(abs(x) - abs(y)) + ' \N{LESS-THAN OR EQUAL TO} ' + str(abs((x) - (y))))
+
+
+
+
