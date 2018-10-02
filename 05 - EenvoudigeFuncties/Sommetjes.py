ff69eae743791c4ffd4b079a0365f058bb5b85b5
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-10-02 18:47:49 +0200

    Sommetjes

--- 05 - EenvoudigeFuncties/Sommetjes.py
+++ 05 - EenvoudigeFuncties/Sommetjes.py
@@ -3,13 +3,15 @@ a = float(input('Geef een waarde voor a: '))
 b = float(input('Geef een waarde voor b: '))
 i = 0
 
-print(str('{:>6.0f}'.format(a * pow(10,i)) ' + ' ('{:<6.0f}'.format(b * pow(10,i)))
+#formule
+print(str('{:>6.0f}'.format(a * pow(10,i))) + ' + ' + ('{:<6.0f}'.format(b * pow(10,i))) + ' = ' + str(int(a * pow(10,i) + b * pow(10,i))))
 i += 1
-print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
+print(str('{:>6.0f}'.format(a * pow(10,i))) + ' + ' + ('{:<6.0f}'.format(b * pow(10,i))) + ' = ' + str(int(a * pow(10,i) + b * pow(10,i))))
 i += 1
-print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
+print(str('{:>6.0f}'.format(a * pow(10,i))) + ' + ' + ('{:<6.0f}'.format(b * pow(10,i))) + ' = ' + str(int(a * pow(10,i) + b * pow(10,i))))
 i += 1
-print(str('{:>6.0f}'.format(a * pow(10,i)) + ('{:<6.0f}'.format(b * pow(10,i)))))
-
+print(str('{:>6.0f}'.format(a * pow(10,i))) + ' + ' + ('{:<6.0f}'.format(b * pow(10,i))) + ' = ' + str(int(a * pow(10,i) + b * pow(10,i))))
+i += 1
+print(str('{:>6.0f}'.format(a * pow(10,i))) + ' + ' + ('{:<6.0f}'.format(b * pow(10,i))) + ' = ' + str(int(a * pow(10,i) + b * pow(10,i))))
 
 
