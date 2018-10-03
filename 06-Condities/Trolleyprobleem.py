a882195de8bb45d8483d4ebeff3327f9dc47d715
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-10-03 11:59:53 +0200

    Trolleyprobleem

---
+++ 06-Condities/Trolleyprobleem.py
@@ -0,0 +1,7 @@
+#Trek aan hendel + Man van brug duwen => Ja + Ja => 2 doden
+#Trek aan hendel : nee + man van brug duwen : ja => 1 dode
+#Trek aan hendel : ja + man van brug duwen : nee => 1 dode
+#Trek aan hendel : nee + man van brug duwen : nee => 5 doden
+
+aantal_doden = input('Aan de hendel trekken?')
+if aantal_doden == 'ja':
