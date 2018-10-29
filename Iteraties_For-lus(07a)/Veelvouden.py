99fc97a9179e6bf7dad65943022859ceeeb56efb
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-10-29 11:49:47 +0100

    Veelvouden

--- Iteraties_For-lus(07a)/Veelvouden.py
+++ Iteraties_For-lus(07a)/Veelvouden.py
@@ -1,4 +1,10 @@
+# r is groter dan 0 en kleiner dan 100 (en natuurlijk getal = integer)
+r = int(input('Geef een getal: '))
+som = 0
 
+# loop
+for x in range(r, 101, r):
+    som += x
 
-
-
+#output
+print(som)
