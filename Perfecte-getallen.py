37e7d8abe6e65bfe745cb417d6827b7a8be515b1
ThibautBlomme <42997698+ThibautBlomme@users.noreply.github.com>
2018-11-14 11:07:00 +0100

    Perfecte getallen

--- Perfecte-getallen.py
+++ Perfecte-getallen.py
@@ -1,5 +1,20 @@
+# variabelen
 n = int(input('Geef een getal: '))
-som = ''
+som = 0
+getallen = ''
 
-for number in som:
-    
+#lus
+for number in range(1, n):
+    if n % number == 0:
+        som += number
+        getallen += ' ' + str(number)
+
+# perfect of niet EN uitzondering 1
+if n == 1:
+    print('{} is geen perfect getal'.format(n))
+
+elif som == n:
+    print('{} is een perfect getal en de delers zijn{}'.format(n, getallen))
+
+else:
+    print('{} is geen perfect getal'.format(n))
